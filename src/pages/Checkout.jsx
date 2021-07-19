393320d5861f05be85b18ac1f0ece4b029836097
Jennifer Ruiz <jennifer.ruiz1871@gmail.com>
2021-07-19 14:48:05 -0300

    Ordenes se envian al backend

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,40 +1,55 @@
 import React from 'react'
-import { Button, Form, Select } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
+import { useCartContext } from '../context/CartContext'
+import { db } from '../Firebase/firebase'
 
 function Checkout() {
-    const options = [
-        { key: 'tc', text: 'Tarjeta de crédito', value: 'tc' },
-        { key: 'td', text: 'Tarjeta de débito', value: 'td' },
-        { key: 'mp', text: 'Mercado Pago', value: 'mp' },
-        { key: 'ef', text: 'Efectivo', value: 'ef' },
-    ]
+    const { cart } = useCartContext()
+
+    const confirm = async () => {
+        const order = {
+            name: document.getElementById('name').value,
+            surname: document.getElementById('surname').value,
+            tel: document.getElementById('tel').value,
+            email: document.getElementById('email').value,
+            address: document.getElementById('address').value,
+            notas: document.getElementById('notas').value,
+            cart: cart,
+        }
+        await db.collection('Orders').doc().set(order)
+    }
+
     return (
         <div className="container">
             <Form className="form">
                 <h1>Realizar pedido</h1>
                 <Form.Field style={{ width: 600 }}>
                     <label>Nombre</label>
-                    <input placeholder="Nombre" />
+                    <input id="name" placeholder="Nombre" />
                 </Form.Field>
                 <Form.Field style={{ width: 600 }}>
                     <label>Apellido</label>
-                    <input placeholder="Apellido" />
+                    <input id="surname" placeholder="Apellido" />
                 </Form.Field>
                 <Form.Field style={{ width: 600 }}>
                     <label>Teléfono</label>
-                    <input type="number" placeholder="Teléfono" />
+                    <input id="tel" type="number" placeholder="Teléfono" />
+                </Form.Field>
+                <Form.Field style={{ width: 600 }}>
+                    <label>Email</label>
+                    <input id="email" placeholder="Email" />
                 </Form.Field>
                 <Form.Field style={{ width: 600 }}>
                     <label>Dirección</label>
-                    <input placeholder="Dirección" />
+                    <input id="address" placeholder="Dirección" />
                 </Form.Field>
-                <Form.Field
-                    control={Select}
-                    label="Método de pago que utilizará en la entrega"
-                    options={options}
-                    placeholder="Método de pago"
-                />
-                <Button type="submit">Realizar pedido</Button>
+                <Form.Field style={{ width: 600 }}>
+                    <label>Notas</label>
+                    <input id="notas" placeholder="Notas" />
+                </Form.Field>
+                <Button type="submit" onClick={confirm}>
+                    Realizar pedido
+                </Button>
             </Form>
         </div>
     )
